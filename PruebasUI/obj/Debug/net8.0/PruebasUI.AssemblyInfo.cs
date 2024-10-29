e52cc5e2bf5e7c3fbcc4f7aed3a193e99993b432
Odalys234 <ae24002@esfe.agape.edu.sv>
2024-10-28 20:58:47 -0600

    Identificadores id

--- PruebasUI/obj/Debug/net8.0/PruebasUI.AssemblyInfo.cs
+++ PruebasUI/obj/Debug/net8.0/PruebasUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebasUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad75ae2c97bbe9529a60b0ea921c0d45f35f01c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671744f73b4087e1cbf87b8d0815e971fa6279b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebasUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebasUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
